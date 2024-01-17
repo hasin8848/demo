3d4545e138a4d5b0f2e53a30db50816b35f728b9
hasin8848 <153600234+hasin8848@users.noreply.github.com>
2024-01-17 12:52:40 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,41 @@
+class A8
+{
+void test_A()
+{
+system.out.printIn("In class A");
+}
+}
+class B8 extends.A8
+{
+void test_8()
+{
+system.out.printIn("In class B");
+}
+}
+class C3 extend B3
+void test_C()
+{
+system.out.printIN("In class C");
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
